65082a4bb4dc61c0a079214114c28dd0237bc04d
SusloffYury <susloff.yury@gmail.com>
2019-11-14 21:55:05 +0300

    last commit

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ console.log(animal.toLowerCase().includes('cat'));
     }
 
 isThereACat('bull, dog, Snake, CaT');
+isThereACat('bull, dog, Snake');
 
 function isPythagoreanTheorem(a,b,c){
     if(Math.pow(parseInt(a),2)+Math.pow(parseInt(b),2)==Math.ceil(Math.pow(parseFloat(c),2))){
@@ -12,12 +13,18 @@ function isPythagoreanTheorem(a,b,c){
     }else{console.log('false')}
   
 }
-//isPythagoreanTheorem(6, 8, 10);
+isPythagoreanTheorem(6, 8, 10);
 isPythagoreanTheorem(2, 8 , 16)
 
 function gentleSum(a,b){
-    if(typeof a=='number'||typeof b=='number'){
-        return a+b
-    }
+   var flag=true;
+  if(flag==isNaN(parseInt(a)+parseInt(b))){
+    console.log( 'please enter valid data');
+  }else{console.log(parseInt(a)+parseInt(b))}
 
-}
+  }
+  
+
+
+gentleSum('234asfasf', '53asfa');
+gentleSum('sd234asfasf', '53asfa');
