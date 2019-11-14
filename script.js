0a6d81be14d2eb72ec490636e6bec6ec9c6b3865
SusloffYury <susloff.yury@gmail.com>
2019-11-14 20:13:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function isThereACat(animal){
+
+console.log(animal.toLowerCase().includes('cat')); 
+
+    }
+
+isThereACat('bull, dog, Snake, CaT');
+
+function isPythagoreanTheorem(a,b,c){
+    if(Math.pow(parseInt(a),2)+Math.pow(parseInt(b),2)==Math.ceil(Math.pow(parseFloat(c),2))){
+        console.log('true');
+    }else{console.log('false')}
+  
+}
+isPythagoreanTheorem(3, 3, 4.24);
+//isPythagoreanTheorem(4, 8 , 9.3)
+
+function gentleSum(){
+
+}
