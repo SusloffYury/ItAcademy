f40873d329418b94c734ce99df86a8f2c9dad11d
SusloffYury <susloff.yury@gmail.com>
2019-11-19 16:44:06 +0300

    task-2

--- script.js
+++ script.js
@@ -1,33 +1,62 @@
-function isThereACat(animal){
+// function isThereACat(animal){
 
-console.log(animal.toLowerCase().includes('cat')); 
+// console.log(animal.toLowerCase().includes('cat')); 
 
-    }
+//     }
 
-isThereACat('bull, dog, Snake, CaT');
-isThereACat('bull, dog, Snake');
+// // isThereACat('bull, dog, Snake, CaT');
+// // isThereACat('bull, dog, Snake');
 
-function isPythagoreanTheorem(a,b,c){
-    if(Math.pow(parseInt(a),2)+Math.pow(parseInt(b),2)==Math.ceil(Math.pow(parseFloat(c),2))){
-        console.log('true');
-    }else{console.log('false')}
+// function isPythagoreanTheorem(a,b,c){
+//     if(Math.pow(parseInt(a),2)+Math.pow(parseInt(b),2)==Math.ceil(Math.pow(parseFloat(c),2))){
+//         console.log('true');
+//     }else{console.log('false')}
   
+// }
+// // isPythagoreanTheorem(6, 8, 10);
+// // isPythagoreanTheorem(2, 8 , 16);
+
+// function gentleSum(a,b){
+  
+//   if(isNaN(parseInt(a)+parseInt(b))){
+//     console.log( 'please enter valid data');
+//   }else{
+//     console.log(parseInt(a)+parseInt(b))
+//   }
+
+//   }
+  
+
+
+// gentleSum('234asfasf', '53asfa');
+// gentleSum('sd234asfasf', '53asfa');
+// gentleSum('234asfasf', 'nmn53asfa');
+
+
+function deleteObjectValueByKey(obj, key){
+
+if (key in obj){
+  delete(obj.key);
+  
+ 
+}else {console.log('null')
 }
-isPythagoreanTheorem(6, 8, 10);
-isPythagoreanTheorem(2, 8 , 16)
 
-function gentleSum(a,b){
-  
-  if(isNaN(parseInt(a)+parseInt(b))){
-    console.log( 'please enter valid data');
-  }else{
-    console.log(parseInt(a)+parseInt(b))
-  }
+}  
 
-  }
-  
+deleteObjectValueByKey({name:'Tanya', age:15},"age");
 
+function printAllEvenNumbers(){
+   let i= prompt('Введите любое число больше 0');
+   if(typeof parseInt(i) =='number'){
+     for(let b = 0;b <= i; b++) {
+     if (b%2==0){
+       alert(b);
+    }
+ }
+}else{   alert('не число');
 
-gentleSum('234asfasf', '53asfa');
-gentleSum('sd234asfasf', '53asfa');
-gentleSum('234asfasf', 'nmn53asfa');
+}
+
+}
+printAllEvenNumbers();
