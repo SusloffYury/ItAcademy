07ef21de912293546f11bd7ebbb8ee76318b799a
Suslov Yury <susloff.yury@gmail.com>
2019-12-21 12:44:20 +0300

    task_8

--- script.js
+++ script.js
@@ -1,83 +1,37 @@
-function sumSalaries(salaries){
-  let sum = 0;
-  
-  // for (let salary of Object.values(salaries)) {
-  //   sum += salary;
-    return Object.values(salaries).reduce((a, b)=> a+b, 0);
-  }
+array = [4,51,26,15,3,4];
 
-  function topSalary(salaries){
-    let maxSal = 0;
-    let maxName = null;
-    for(let[key, value] of Object.entries(salaries)){
-      if(value > maxSal){
-        maxSal = value;
-        maxName = key;
-      }
+//console.log(array.fill(3,1,4));
+
+Array.prototype.max = function(){
+   return array.sort((a,b)=> a-b).pop();
     }
-    
-     return maxName;
-    } 
-     
-     function TopSalaryIt(salaries){
-        let cell = Object.entries(salaries)
-        .sort(function([ ,salA ],[ ,salB ]){ return salA - salB}) 
-        .pop();
-        return cell[0];
-     }
-  
+console.log(array.max());
 
-function printNumbers(from, to){ 
-        let time = from;
-   let countNumber = setInterval(function(){
-      console.log(time);
-      if(time == to){
-        clearInterval(countNumber);
-      }
-    time++;
-  }, 1000);
+if(!Array.prototype.fill){
+    Array.prototype.fill = function(){
+        this.fill();
+    }
 }
 
-function printNumbersRec(from, to) {
-  let time = from;
-
-  setTimeout(function timer() {
-    console.log(time);
-    if (time < to) {
-      setTimeout(timer, 1000);
-    }
-    time++;
-  }, 1000);
+function askPassword(ok, fail) {
+  let password = prompt("Password?", '');
+  if (password == "rockstar") ok();
+  else fail();
 }
 
-function timers(){
-    let i = 0;
-    setTimeout(()=>console.log(i), 10000);
-    for(let j = 0; j < 1000000000; j++){
-      i++;
-  }
-}
+let user = {
+  name : 'Vasya',
 
- function aclean(arr){
-   let map = new Map();
-    for(let string of arr){
-     let key =  string.split("").sort().join("");
-     map.set(key, string);
-    }
-  return Array.from(map.values());
-   
- }
- let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
- 
-let salaries = {
-  "John": 100,
-  "Pete": 300,
-  "Mary": 250
+  loginOk() {
+    alert(`${user.name} logged in`);
+  },
+
+  loginFail() {
+    alert(`${user.name} failed to log in`);
+  },
+
 };
-//console.log(sumSalaries(salaries));
-//console.log(topSalary(salaries));
-//printNumbers(5, 10);
-//printNumbersRec(1, 6);
-//timers();
-//console.log(aclean(arr));
-console.log(TopSalaryIt(salaries));
+askPassword(user.loginOk, user.loginFail);
+     
+    
+  
