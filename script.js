ce7856cf861eda58f2a07be5f5a1cff26c06c612
Suslov Yury <susloff.yury@gmail.com>
2019-12-21 20:55:21 +0300

    f

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-let data = {
-  "Рыбы": ['Форель', 'Лосось'],
-  "Деревья": ["секвойя", "дуб"],
-  "Цветковые": ["яблоня","магнолия"]
-};
-
-
-  function tagCreat(obj){
-   Object.entries(obj)
-     .map((el)=>{
-        ol = document.createElement('ol')
-        ol.className = el[0] 
-        ol.innerHTML = el[0]
-          el[1].forEach(elem=>{
-          let li = document.createElement('li')
-           li.className = elem
-           li.innerHTML = elem
-           document.body.append(ol)
-           ol.append(li)
-          }) 
-      })
-     
+//https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json
+const SUPER_HEROES_CONT = document.querySelector('.super_heroes')
+let super_heroes=()=>{
+return fetch("https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json")
+ .then(response=>response.json())
+  .then(heroes_array=>{
+    heroes_array.length = 10;
+    return heroes_array;
+  });
  }
-  
+const creatSuperHeroesHTML =({name, images: { lg: heroImage}})=>{
+  let div = document.createElement('div');
+    div.className = name;
+    div.innerHTML = name;
+     let image = document.createElement('img')
+     image.src = heroImage;
+     div.append(image);
+     SUPER_HEROES_CONT.append(div); 
+};
+super_heroes().then(super_heroes=>{
+  super_heroes.map(creatSuperHeroesHTML)
+}) 
 
-  tagCreat(data);
