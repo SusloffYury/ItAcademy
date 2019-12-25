18219f1f12edf386d8a0154743ef71ff2b010b33
Suslov Yury <susloff.yury@gmail.com>
2019-12-25 15:26:58 +0300

    setInterval

--- script.js
+++ script.js
@@ -9,26 +9,44 @@ return fetch("https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json")
  }
 const creatSuperHeroesHTML =({name, images: { lg: heroImage}})=>{
   let div = document.createElement('div');
-  let title = document.createElement('div');
-  title.className = 'title'
-  title.innerHTML = name;
-   div.className = 'superhero';
-   div.className = 'superheroVisible'
-    let image = document.createElement('img')
-     image.src = heroImage;
-    
-     div.append(image);
-     div.append(title);
-     
-     SUPER_HEROES_CONT.append(div); 
+     div.className = 'superhero';
+     let image = document.createElement('img')
+      image.src = heroImage;
+      div.append(image);
+      SUPER_HEROES_CONT.append(div); 
    
 };
+    const createArray =()=>{
+    return document.querySelectorAll('.superhero')
+    };
 
+  const creatGallery =(galleryArray)=>{
+    let arrayIndex = 0;
+    setInterval(() => {
+      if(!galleryArray[arrayIndex]){
+        arrayIndex = 0;
+        galleryArray[galleryArray.length - 1].classList.toggle('superheroVisible')
+      }
 
-super_heroes().then(super_heroes=>{
-  super_heroes.map(creatSuperHeroesHTML)
+      if(galleryArray[arrayIndex - 1]){
+        galleryArray[arrayIndex - 1].classList.toggle('superheroVisible')
+       
+    }
+      if(galleryArray[arrayIndex]){
+             galleryArray[arrayIndex ].classList.toggle('superheroVisible')
+             arrayIndex++;
+      }
+    }, 1000);
+    
+
+  } 
+
  
-})
+ super_heroes().then(super_heroes=>{
+  super_heroes.map(creatSuperHeroesHTML)
+ }).then(createArray)
+ .then((galleryArray)=>creatGallery(galleryArray))
+ 
 
 
 
