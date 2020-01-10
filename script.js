cb7412faae4c07ba28a791148b38a5561b7f1caf
Suslov Yury <susloff.yury@gmail.com>
2020-01-10 16:47:15 +0300

    nextButt

--- script.js
+++ script.js
@@ -22,42 +22,46 @@ const creatSuperHeroesHTML =({images: { lg: heroImage}})=>{
      return document.querySelectorAll('.superhero')
         
   }
-         
-
-      let button = (galleryArray)=>{
-         let buttNext = document.getElementById('next')
-         let buttPrev = document.getElementById('prev') 
-            
-         let arrayIndex = 0;
-           // galleryArray[arrayIndex].classList.toggle('superheroVisible')
-          let next =()=>{
-                      
-                     if(galleryArray[arrayIndex - 1]){
-                        galleryArray[arrayIndex - 1].classList.toggle('superheroVisible')
-                     }
-
-                        if(galleryArray[arrayIndex]){
-                           galleryArray[arrayIndex].classList.toggle('superheroVisible')
-                           arrayIndex++;
-                          
-                        }
-
-                        if(!galleryArray[arrayIndex + 1]){
-                           arrayIndex = 0;
-                         }
-                           
-                 
-      };
-      buttNext.addEventListener('click', next)
-      buttPrev.addEventListener('click', prev)
-     }
-      
-      
+ 
+ let buttons = (galleryArray)=>{
+  // galleryArray[arrayIndex].classList.toggle('superheroVisible')
+   let arrayIndex = 0;
+   let prevIndex = 0; 
+             
+    document.addEventListener('click',function(event){
+     let target = event.target;
+      if(target.id == 'next'){   
+       if(arrayIndex > galleryArray.length-1){
+        arrayIndex = 0;
+        }
+        console.log(arrayIndex)
+        if(arrayIndex==0){
+         galleryArray[arrayIndex].classList.toggle('superheroVisible')
+         }
+         if(galleryArray[arrayIndex-1]){
+          galleryArray[arrayIndex-1].classList.toggle('superheroVisible')
+          galleryArray[arrayIndex].classList.toggle('superheroVisible')
+            }
+           if(!galleryArray[arrayIndex + 1]){
+           galleryArray[arrayIndex].classList.toggle('superheroVisible')
+           }
+          arrayIndex++;
+            }
+           if(target.id =='prev'){
+            if(arrayIndex <= 0){
+            arrayIndex = galleryArray.length - 1
+              }
+           console.log(arrayIndex)
+            arrayIndex--;
+          }
+                  
+        })
+       }
 
 super_heroes().then(super_heroes=>{
    super_heroes.map(creatSuperHeroesHTML)
    }).then((galleryArray)=>creatGalleryArr(galleryArray))
-   .then((galleryArray)=>button(galleryArray))
+   .then((galleryArray)=>buttons(galleryArray))
   
    
       
